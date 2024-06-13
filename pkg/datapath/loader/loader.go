12d193a515124619b3bb88cc57862bc1be40ab91
Timo Beckers <timo@isovalent.com>
2024-06-13 17:10:03 +0200

    bpf,loader: explicitly manage policy programs for bpf_lxc and bpf_host
    Remove the hack from loadDatapath that pulled out policy maps and programs
    before loading a Collection. Explicitly commit them to the global policy maps
    as part of loading the endpoint after getting all internal state in order first.
    
    Also removes the POLICY_CALL_MAP macro to ensure stable map name in
    CollectionSpec. POLICY_CALL_MAP would evaluate to test_cilium_policy_calls in tests,
    while the agent would build the datapath with the macro set to cilium_policy_calls.
    This is not compatible with the new way of loading ELFs, so the macro was removed.
    
    The reason for the test_ map prefixes is not documented. With verifier
    tests now unflagging maps for pinning, and the loader test suite taking
    ownership of /sys/fs/bpf/{cilium,tc}, it doesn't look like there's a strong
    reason for keeping the prefixes around. Removing all map name macros is a lot
    of churn, so this will follow as its own change set at a later time.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- pkg/datapath/loader/loader.go
+++ pkg/datapath/loader/loader.go
@@ -333,6 +333,11 @@ func (l *loader) reloadHostDatapath(ep datapath.Endpoint, spec *ebpf.CollectionS
 	}
 	defer coll.Close()
 
+	// Insert host endpoint policy program.
+	if err := coll.Maps[policymap.PolicyCallMapName].Update(uint32(ep.GetID()), coll.Programs["handle_lxc_traffic"], ebpf.UpdateAny); err != nil {
+		return fmt.Errorf("inserting host endpoint policy program: %w", err)
+	}
+
 	// Attach cil_to_host to cilium_host ingress.
 	if err := attachSKBProgram(host, coll.Programs[symbolToHostEp], symbolToHostEp,
 		bpffsDeviceLinksDir(bpf.CiliumPath(), host), netlink.HANDLE_MIN_INGRESS, option.Config.EnableTCX); err != nil {
@@ -447,23 +452,6 @@ func (l *loader) reloadHostDatapath(ep datapath.Endpoint, spec *ebpf.CollectionS
 func (l *loader) reloadDatapath(ep datapath.Endpoint, spec *ebpf.CollectionSpec) error {
 	device := ep.InterfaceName()
 
-	// Replace all occurrences of the template endpoint ID with the real ID.
-	for _, name := range []string{
-		policymap.PolicyCallMapName,
-		policymap.PolicyEgressCallMapName,
-	} {
-		pm, ok := spec.Maps[name]
-		if !ok {
-			continue
-		}
-
-		for i, kv := range pm.Contents {
-			if kv.Key == (uint32)(templateLxcID) {
-				pm.Contents[i].Key = (uint32)(ep.GetID())
-			}
-		}
-	}
-
 	if ep.IsHost() {
 		devices := l.nodeConfig.Load().DeviceNames()
 
@@ -480,6 +468,23 @@ func (l *loader) reloadDatapath(ep datapath.Endpoint, spec *ebpf.CollectionSpec)
 			return err
 		}
 		defer coll.Close()
+
+		// Insert policy programs before attaching entrypoints to tc hooks.
+		// Inserting a policy program is considered an attachment, since it makes
+		// the code reachable by bpf_host when it evaluates policy for the endpoint.
+		// All internal tail call plumbing needs to be done before this point.
+		// If the agent dies uncleanly after the first program has been inserted,
+		// the endpoint's connectivity will be partially broken or exhibit undefined
+		// behaviour like missed tail calls or drops.
+		if err := coll.Maps[policymap.PolicyCallMapName].Update(uint32(ep.GetID()), coll.Programs["handle_policy"], ebpf.UpdateAny); err != nil {
+			return fmt.Errorf("inserting endpoint policy program: %w", err)
+		}
+
+		if prog := coll.Programs["handle_policy_egress"]; prog != nil {
+			if err := coll.Maps[policymap.PolicyEgressCallMapName].Update(uint32(ep.GetID()), prog, ebpf.UpdateAny); err != nil {
+				return fmt.Errorf("inserting endpoint egress policy program: %w", err)
+			}
+		}
 
 		iface, err := netlink.LinkByName(device)
 		if err != nil {
