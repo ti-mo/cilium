df3d703cdd6c461a506d2d79e853749337f0fc48
Timo Beckers <timo@isovalent.com>
2025-04-01 17:21:55 +0200

    datapath: remove interface_ifindex declaration in WriteNodeConfig
    This is a leftover from before the decision to add a global.h file
    to declare datapath configuration.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- pkg/datapath/linux/config/config.go
+++ pkg/datapath/linux/config/config.go
@@ -661,9 +661,6 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 	fmt.Fprint(fw, declareConfig("identity_length", identity.GetClusterIDShift(), "Identity length in bits"))
 	fmt.Fprint(fw, assignConfig("identity_length", identity.GetClusterIDShift()))
 
-	fmt.Fprint(fw, declareConfig("interface_ifindex", uint32(0), "ifindex of the interface the bpf program is attached to"))
-	cDefinesMap["THIS_INTERFACE_IFINDEX"] = "CONFIG(interface_ifindex)"
-
 	// --- WARNING: THIS CONFIGURATION METHOD IS DEPRECATED, SEE FUNCTION DOC ---
 
 	// Since golang maps are unordered, we sort the keys in the map
