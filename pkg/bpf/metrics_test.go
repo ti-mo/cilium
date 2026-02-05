eddba183eec98df2bea96029c81e856fae533b85
Timo Beckers <timo@isovalent.com>
2026-02-05 14:58:09 +0100

    bpf/metrics: RegisterTablePressureMetricsJob input validation
    There are currently no constraints on what kind of interface values
    can be provided to RegisterTablePressureMetricsJob. If the caller
    passes an uninitialized map by-value, it can never be populated after
    the fact, and likely points at a programming error.
    
    Take a nil Map input as a signal that the map is disabled in its provider.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

---
+++ pkg/bpf/metrics_test.go
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package bpf
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type testMap struct {
+	m *uint8
+}
+
+func (tm testMap) IsOpen() bool            { return false }
+func (tm testMap) NonPrefixedName() string { return "" }
+func (tm testMap) MaxEntries() uint32      { return 0 }
+
+type testMapPtr struct {
+	m *uint8
+}
+
+func (tm *testMapPtr) IsOpen() bool            { return false }
+func (tm *testMapPtr) NonPrefixedName() string { return "" }
+func (tm *testMapPtr) MaxEntries() uint32      { return 0 }
+
+func TestValidMap(t *testing.T) {
+	assert.Error(t, validProvidedMap(testMap{}))
+	v := uint8(1)
+	assert.NoError(t, validProvidedMap(testMap{&v}))
+
+	assert.ErrorIs(t, validProvidedMap[*testMapPtr](nil), errMapDisabled)
+	assert.NoError(t, validProvidedMap(&testMapPtr{}))
+}
